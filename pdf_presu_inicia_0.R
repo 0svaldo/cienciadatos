0003d9375ec3a52f3fc06445fe95bd86ab2e626a
Osvaldo <osvaldo.larancuent@gmail.com>
2017-05-10 00:17:13 -0400

    Update pdf_presu_inicia_0.R

--- pdf_presu_inicia_0.R
+++ pdf_presu_inicia_0.R
@@ -8,13 +8,17 @@
 #1. Libreria y de Paquetes 
 #install.packages("pdftools")
 library("pdftools",logical.return = TRUE)
+print("Libreria pdftools .... cargada")
+
 #install.packages("stringr")
 library("stringr",logical.return = TRUE)
+print("Libreria stringr .... cargada")
 
 #2. Definicion de Variables 
 miUrlLectura <- "http://www.digepres.gob.do/wp-content/uploads/2017/04/Informe-primer-trimestre.pdf"
 miFicheroPdf<-"./data/presu-01-2017.pdf"
 miFicheroCSV<-"./data/presu-01-2017.csv"
+print("Nombres de archivos cargados .... cargada")
 
 miDataDir <- "data"
 miFechaDescarga <- date()
@@ -25,3 +29,4 @@ miNuevoPath <- "~/Documents/Programacion/r programming/Scrap HTML/CienciaDatos"
 
 #3. Directorio y Working Space
 setwd(miNuevoPath)
+print("Directorio definido .... Listo")
